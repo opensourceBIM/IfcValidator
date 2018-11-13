0a39f2e956c4d775ea76dd51b1ff89f74de0958c
Ruben de Laat <ruben@logic-labs.nl>
2018-11-13 20:48:25 +0100

    Use ILS registry

--- src/org/bimserver/ifcvalidator/BcfIfcValidatorPlugin.java
+++ src/org/bimserver/ifcvalidator/BcfIfcValidatorPlugin.java
@@ -1,23 +1,6 @@
 package org.bimserver.ifcvalidator;
 
-/******************************************************************************
- * Copyright (C) 2009-2018  BIMserver.org
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- * 
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see {@literal<http://www.gnu.org/licenses/>}.
- *****************************************************************************/
-
-import org.bimserver.ifcvalidator.checks.FullModelCheckerRegistry;
+import org.bimserver.ifcvalidator.checks.IlsModelCheckerRegistry;
 import org.bimserver.plugins.SchemaName;
 
 /******************************************************************************
@@ -42,7 +25,7 @@ import org.bimserver.validationreport.IssueContainerSerializer;
 public class BcfIfcValidatorPlugin extends AbstractIfcValidatorPlugin {
 
 	public BcfIfcValidatorPlugin() {
-		super(SchemaName.BCF_ZIP_2_0, false, new FullModelCheckerRegistry());
+		super(SchemaName.BCF_ZIP_2_0, false, new IlsModelCheckerRegistry());
 	}
 
 	@Override
